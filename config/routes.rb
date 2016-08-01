58ba3af3e004e7577012e7648a3c3d8fd04a35a9
jessonfoo <jessonfoo@gmail.com>
2016-08-01 12:37:39 -0700

    change root to home#index, added stumble link

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  root 'contents#index'
+  root 'home#index'
   resources :links
   resources :contents
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
